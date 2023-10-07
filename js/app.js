ef993b41e8565ea8efe779e307af21b60d51e403
pgpavlides <georgepavlideswork@gmail.com>
2023-10-07 18:01:18 +0300

    first commit

--- js/app.js
+++ js/app.js
@@ -4,80 +4,68 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import vertexShader from './shaders/vertex.glsl';
 import fragmentShader from './shaders/fragment.glsl';
 
-export default class Sketch{
-    constructor(options){
+class Sketch {
+    constructor(options) {
         this.container = options.dom;
         this.scene = new THREE.Scene();
 
         this.width = this.container.offsetWidth;
         this.height = this.container.offsetHeight;
 
-        this.renderer = new THREE.WebGLRenderer({
-            alpha: true
-        });
+        this.renderer = new THREE.WebGLRenderer({ canvas: this.container, alpha: true });
         this.renderer.setSize(this.width, this.height);
-        this.container.appendChild(this.renderer.domElement);
 
-       
-
-        this.camera = new THREE.PerspectiveCamera(70, this.width / this.height, 0.01, 10);
-        this.camera.position.z = 1;
+        this.camera = new THREE.OrthographicCamera(this.width / -2, this.width / 2, this.height / 2, this.height / -2, 1, 1000);
+        this.camera.position.z = 10;
 
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.enableZoom = false;
+        this.controls.enablePan = false;
+        this.controls.enableRotate = false;
 
         this.time = 0;
 
         this.addObjects();
-        this.setupResize()
+        this.setupResize();
         this.render();
     }
 
-    setupResize(){
+    setupResize() {
         window.addEventListener('resize', this.resize.bind(this));
     }
 
-    resize(){
+    resize() {
         this.width = this.container.offsetWidth;
         this.height = this.container.offsetHeight;
-
         this.renderer.setSize(this.width, this.height);
-        this.camera.aspect = this.width / this.height;
-
+        this.camera.left = this.width / -2;
+        this.camera.right = this.width / 2;
+        this.camera.top = this.height / 2;
+        this.camera.bottom = this.height / -2;
         this.camera.updateProjectionMatrix();
     }
 
-    addObjects(){
-        this.geometry = new THREE.PlaneGeometry(1,0.05,10);
-        this.material = new THREE.MeshNormalMaterial({ side: THREE.DoubleSide });
-        this.material.wireframe = true;
-
-        this.invMesh = new THREE.Mesh(this.geometry,this.material)
-
+    addObjects() {
+        this.geometry = new THREE.PlaneGeometry(this.width, this.height);
         this.material = new THREE.ShaderMaterial({
             uniforms: {
-                time: {value: 0}
+                time: { value: 0 }
             },
             vertexShader: vertexShader,
             fragmentShader: fragmentShader,
-        })
-
-        this.mesh = new THREE.Points(this.geometry, this.material);
-        
-        this.scene.add(this.mesh,this.invMesh);
+        });
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.scene.add(this.mesh);
     }
 
-    render(){
-        this.time += 0.005;
-
-        // this.material.uniforms.time.value = this.time;
-
-
+    render() {
+        this.time += 0.05;
+        this.material.uniforms.time.value = this.time;
         this.renderer.render(this.scene, this.camera);
-
         window.requestAnimationFrame(this.render.bind(this));
     }
 }
 
 new Sketch({
     dom: document.getElementById('container')
-})
+});
--- js/shaders/fragment.glsl
+++ js/shaders/fragment.glsl
@@ -1,5 +1,12 @@
 varying vec2 vUv;
+uniform float time; // Assuming you are passing time from your application to the shader.
 
 void main() {
-    gl_FragColor = vec4( vUv,0., 1.0 );
+    // Convert UV coordinates to range between 0 and 1
+    vec2 uv = vUv;
+//     vec2 uv = fragCoord / iResolution.xy;
+
+    // Time varying pixel color using the same logic from the mainImage function
+
+    gl_FragColor = vec4(uv.x, 0.0, 0.0, 1.0);
 }
